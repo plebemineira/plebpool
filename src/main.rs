9f192e75467960c389007905e34c132c3160a8b8
plebhash <plebhash@proton.me>
2023-11-28 19:24:15 -0300

    bootstrap

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+pub use pool_sv2::lib::*;
+pub use jd_server::lib::*;
+
+fn main() {
+    println!("⛏️ plebs be hashin ⚡");
+    // opinionated share accounting logic using pool_sv2::lib
+    // opinionated coinbase generation logic using jd_server::lib
+}
