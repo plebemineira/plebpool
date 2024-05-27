8cd5b14f4280a16605d71f75280ff128fafa16e9
plebhash <plebhash@proton.me>
2024-05-27 01:45:28 -0300

    fmt

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-use serde::{Deserialize, Serialize};
 use crate::{ln, pool};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct PlebPoolConfig {
--- src/pool/service/mod.rs
+++ src/pool/service/mod.rs
@@ -6,15 +6,17 @@ use crate::pool::service::downstream::DownstreamService;
 use tracing::info;
 
 pub struct PoolService {
-    downstream_service: DownstreamService
+    downstream_service: DownstreamService,
 }
 
 impl PoolService {
     pub async fn new(pool_config: PoolConfig) -> anyhow::Result<Self> {
-        let downstream_service = DownstreamService::new(pool_config.downstream_listen_host, pool_config.downstream_listen_port).await?;
-        Ok(Self {
-            downstream_service
-        })
+        let downstream_service = DownstreamService::new(
+            pool_config.downstream_listen_host,
+            pool_config.downstream_listen_port,
+        )
+        .await?;
+        Ok(Self { downstream_service })
     }
 
     pub async fn serve(self) -> anyhow::Result<tokio::task::JoinHandle<anyhow::Result<()>>> {
