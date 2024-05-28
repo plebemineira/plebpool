35076db26355969904c43045187916308538a295
plebhash <plebhash@proton.me>
2024-05-27 23:30:34 -0300

    init submodule

--- stratum-message-generator.sh
+++ stratum-message-generator.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git submodule update --init --recursive
+
 MG_MANIFEST_PATH=stratum/utils/message-generator/Cargo.toml
 
 RUST_LOG=info cargo run --manifest-path=$MG_MANIFEST_PATH -- stratum-message-generator/downstream-setup-connection.json
